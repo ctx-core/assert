042f4cf8b7fb9b51caa9d69bce147ae18000e38c
Brian Takita <brian.takita@gmail.com>
2021-05-15 10:05:25 -0400

    ## 4.1.0
    ### Minor Changes
    
    - "noImplicitAny": true

--- src/assert_keys.ts
+++ src/assert_keys.ts
@@ -1,10 +1,10 @@
-export function assert_keys(
-	obj,
-	_failure =
-		(value, key)=>
+export function assert_keys<Val extends unknown = unknown>(
+	obj:Record<string, Val>,
+	_failure:((value:any, key:string)=>false|string) =
+		(value:any, key:string)=>
 			!value && `${key} is not present`
 ) {
-	const failures = {}
+	const failures:Record<string, string> = {}
 	let has_failure
 	for (let key in obj) {
 		const failure = _failure(obj[key], key)
