3e185b5dff90c794e05bb841331fc29495dc9671
Brian Takita <brian.takita@gmail.com>
2021-05-25 01:29:10 -0400

    ## 4.3.0
    ### Minor Changes
    
    - fix tsc build directory issues

--- src/assert_keys.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function assert_keys<Val extends unknown = unknown>(obj: Record<string, Val>, _failure?: ((value: any, key: string) => false | string)): void;
-export { assert_keys as assert__keys };
