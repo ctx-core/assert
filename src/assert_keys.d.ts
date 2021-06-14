4777a2b9f6025659fbcadb5e10210f1db0a39418
Brian Takita <brian.takita@gmail.com>
2021-06-14 04:31:00 -0400

    ## 5.0.0
    ### Major Changes
    
    - move from dist to src directory

---
+++ src/assert_keys.d.ts
@@ -0,0 +1,2 @@
+export declare function assert_keys<Val extends unknown = unknown>(obj: Record<string, Val>, _failure?: ((value: any, key: string) => false | string)): void;
+export { assert_keys as assert__keys };
---
+++ src/index.js
@@ -0,0 +1,2 @@
+export * from './assert_keys';
+//# sourceMappingURL=src/index.js.map
